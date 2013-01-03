133268b81cd3a48de39111a53482c6f9289b687f
Michael Schidlowski <micha@cosmonovo.(none)>
2013-01-03 22:45:59 +0100

    fixes.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ms_entity_validation');
+        $rootNode = $treeBuilder->root('mrm_entity_validation');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
--- DependencyInjection/MrMEntityValidationExtension.php
+++ DependencyInjection/MrMEntityValidationExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class MSEntityValidationExtension extends Extension
+class MrMEntityValidationExtension extends Extension
 {
     /**
      * {@inheritDoc}
